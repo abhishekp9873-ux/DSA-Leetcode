2132a3d2ea1f155f628faf0cc0c3795af0946fd8
Abhishek Pal <abhishek.p9873@gmail.com>
2026-02-18 20:11:46 +0530

    Find Peak Element

---
+++ PeakElement(162)LC.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+       int n=nums.length;
+        int L=0;
+        int R=n-1;
+       
+        while(L<R){
+           int mid= L+(R-L)/2;
+            if(nums[mid]>nums[mid+1]){
+                R= mid;
+            }else{
+                L=mid+1;
+            }
+
+        }
+        return L;
+    }
+}
